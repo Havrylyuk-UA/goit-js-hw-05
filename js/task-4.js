db5c2745d61b8a6803f51cdfcbd4d430d9b1aace
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2023-11-01 13:55:10 +0200

    fix massive

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,7 @@ const getTotalBalanceByGender = (users, gender) => users.filter(user => user.gen
                                                             return total + user.balance;
                                                         }, 0);
 
-const allUsers = [
+const allUsersB = [
     {
     name: "Moore Hensley",
     gender: "male",
@@ -41,6 +41,6 @@ const allUsers = [
   }
 ];
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+console.log(getTotalBalanceByGender(allUsersB, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(allUsersB, "female")); // 8863
